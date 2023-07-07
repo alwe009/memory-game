7a768fe997dfbd7d26b0549a549fee4546990bcb
alwe009 <63882279+alwe009@users.noreply.github.com>
2023-07-07 20:46:29 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Cards from "./components/Cards";
+
+function App() {
+  return (
+    <>
+      <h1>Memory Game React JS</h1>
+      <div className="container"></div>
+      <Cards />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const Cards = () => {
+  const [items, setItems] = useState();
+  return <div>Cards</div>;
+};
+
+export default Cards;
